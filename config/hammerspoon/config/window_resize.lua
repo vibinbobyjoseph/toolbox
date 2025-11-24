d844210521e5f214bf4bbdcc49b5dd6e60c89638
Vibin Joseph <vibinbobyjoseph@gmail.com>
2025-11-24 12:11:31 +0000

    The quarter-screen positioning feature

--- config/hammerspoon/config/window_resize.lua
+++ config/hammerspoon/config/window_resize.lua
@@ -4,10 +4,74 @@
 local hyperWR = {"ctrl", "alt"}
 local utils = require("config.utils")
 
--- Move window to the left half of the screen
+-- Quarter-screen positioning using arrow key combinations
+-- Hold Ctrl+Alt, then press Left+Up for top-left, etc.
+
+-- Track last arrow pressed for compound operations
+local lastArrow = nil
+local arrowTimer = nil
+
+local function resetArrowState()
+    lastArrow = nil
+    if arrowTimer then
+        arrowTimer:stop()
+        arrowTimer = nil
+    end
+end
+
+local function handleQuarterScreen(firstArrow, secondArrow)
+    local win = utils.getActiveWindow()
+    if not win then
+        hs.alert.show("No window available")
+        return
+    end
+
+    local screen = win:screen():frame()
+    local x, y, w, h = screen.x, screen.y, screen.w / 2, screen.h / 2
+
+    -- Determine position based on arrow combination
+    if firstArrow == "left" and secondArrow == "up" then
+        -- Top-left
+        win:setFrame({x = x, y = y, w = w, h = h})
+    elseif firstArrow == "right" and secondArrow == "up" then
+        -- Top-right
+        win:setFrame({x = x + w, y = y, w = w, h = h})
+    elseif firstArrow == "left" and secondArrow == "down" then
+        -- Bottom-left
+        win:setFrame({x = x, y = y + h, w = w, h = h})
+    elseif firstArrow == "right" and secondArrow == "down" then
+        -- Bottom-right
+        win:setFrame({x = x + w, y = y + h, w = w, h = h})
+    elseif firstArrow == "up" and secondArrow == "left" then
+        -- Top-left (reverse order)
+        win:setFrame({x = x, y = y, w = w, h = h})
+    elseif firstArrow == "up" and secondArrow == "right" then
+        -- Top-right (reverse order)
+        win:setFrame({x = x + w, y = y, w = w, h = h})
+    elseif firstArrow == "down" and secondArrow == "left" then
+        -- Bottom-left (reverse order)
+        win:setFrame({x = x, y = y + h, w = w, h = h})
+    elseif firstArrow == "down" and secondArrow == "right" then
+        -- Bottom-right (reverse order)
+        win:setFrame({x = x + w, y = y + h, w = w, h = h})
+    end
+
+    resetArrowState()
+end
+
+-- Modified left arrow with quarter-screen support
 hs.hotkey.bind(hyperWR, "left", function()
     local win = utils.getActiveWindow()
-    if win then
+    if not win then
+        hs.alert.show("No window available")
+        return
+    end
+
+    if lastArrow and (lastArrow == "up" or lastArrow == "down") then
+        -- Quarter screen operation
+        handleQuarterScreen(lastArrow, "left")
+    else
+        -- Normal left half operation
         local screen = win:screen():frame()
         win:setFrame({
             x = screen.x,
@@ -15,15 +79,27 @@ hs.hotkey.bind(hyperWR, "left", function()
             w = screen.w / 2,
             h = screen.h
         })
-    else
-        hs.alert.show("No window available")
+
+        -- Set state for potential quarter screen
+        lastArrow = "left"
+        if arrowTimer then arrowTimer:stop() end
+        arrowTimer = hs.timer.doAfter(0.5, resetArrowState)
     end
 end)
 
--- Move window to the right half of the screen
+-- Modified right arrow with quarter-screen support
 hs.hotkey.bind(hyperWR, "right", function()
     local win = utils.getActiveWindow()
-    if win then
+    if not win then
+        hs.alert.show("No window available")
+        return
+    end
+
+    if lastArrow and (lastArrow == "up" or lastArrow == "down") then
+        -- Quarter screen operation
+        handleQuarterScreen(lastArrow, "right")
+    else
+        -- Normal right half operation
         local screen = win:screen():frame()
         win:setFrame({
             x = screen.x + screen.w / 2,
@@ -31,15 +107,27 @@ hs.hotkey.bind(hyperWR, "right", function()
             w = screen.w / 2,
             h = screen.h
         })
-    else
-        hs.alert.show("No window available")
+
+        -- Set state for potential quarter screen
+        lastArrow = "right"
+        if arrowTimer then arrowTimer:stop() end
+        arrowTimer = hs.timer.doAfter(0.5, resetArrowState)
     end
 end)
 
--- Move window to the top half of the screen
+-- Modified up arrow with quarter-screen support
 hs.hotkey.bind(hyperWR, "up", function()
     local win = utils.getActiveWindow()
-    if win then
+    if not win then
+        hs.alert.show("No window available")
+        return
+    end
+
+    if lastArrow and (lastArrow == "left" or lastArrow == "right") then
+        -- Quarter screen operation
+        handleQuarterScreen(lastArrow, "up")
+    else
+        -- Normal top half operation
         local screen = win:screen():frame()
         win:setFrame({
             x = screen.x,
@@ -47,15 +135,27 @@ hs.hotkey.bind(hyperWR, "up", function()
             w = screen.w,
             h = screen.h / 2
         })
-    else
-        hs.alert.show("No window available")
+
+        -- Set state for potential quarter screen
+        lastArrow = "up"
+        if arrowTimer then arrowTimer:stop() end
+        arrowTimer = hs.timer.doAfter(0.5, resetArrowState)
     end
 end)
 
--- Move window to the bottom half of the screen
+-- Modified down arrow with quarter-screen support
 hs.hotkey.bind(hyperWR, "down", function()
     local win = utils.getActiveWindow()
-    if win then
+    if not win then
+        hs.alert.show("No window available")
+        return
+    end
+
+    if lastArrow and (lastArrow == "left" or lastArrow == "right") then
+        -- Quarter screen operation
+        handleQuarterScreen(lastArrow, "down")
+    else
+        -- Normal bottom half operation
         local screen = win:screen():frame()
         win:setFrame({
             x = screen.x,
@@ -63,8 +163,11 @@ hs.hotkey.bind(hyperWR, "down", function()
             w = screen.w,
             h = screen.h / 2
         })
-    else
-        hs.alert.show("No window available")
+
+        -- Set state for potential quarter screen
+        lastArrow = "down"
+        if arrowTimer then arrowTimer:stop() end
+        arrowTimer = hs.timer.doAfter(0.5, resetArrowState)
     end
 end)
 
